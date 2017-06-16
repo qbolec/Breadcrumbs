afbbf16ea7a192bd49c7098cbade215593fd9f24
qbolec <qbolec@gmail.com>
2017-06-16 21:20:55 +0200

    Speedup by caching line range queries
    These changes gave 5x speedup on Sublime Text 2, by eliminating multiple
    redundant calls to view.line, view.text_pos etc. Also it helped to check
    if a line contains non-white characters from right to left, instead of
    usual left-to-right. Another gain was from reversing the order of
    checks: first check if the line has smaller indend than previous, and
    only then check if it contains non-whites.

--- Breadcrumbs.py
+++ Breadcrumbs.py
@@ -10,18 +10,9 @@ except NameError:
 def get_tab_size(view):
   return int(view.settings().get('tab_size', 8))
 
-def get_line_by_row(view, row):
-  return view.line(view.text_point(row,0))
-
-def points_of_line(line):
-  return xrange(line.begin(), line.end())
-
-def get_row_indentation(view, row):
-  tab_size = get_tab_size(view)
+def get_row_indentation(points, view, tab_size, limit=1e20):
   pos = 0
-  ln = get_line_by_row(view,row)
-
-  for pt in points_of_line(ln):
+  for pt in points:
     ch = view.substr(pt)
 
     if ch == '\t':
@@ -33,85 +24,88 @@ def get_row_indentation(view, row):
     else:
       break
 
+    if limit <= pos:
+      break
+
   return pos
 
-def is_white_row(view, row):
-  line = get_line_by_row(view,row)
-  return all(view.substr(pt).isspace() for pt in points_of_line(line))
+def is_white_row(view, points):
+  return all(view.substr(pt).isspace() for pt in reversed(points))
 
-def get_breadcrumb(view, line, limit):
-  for pt in points_of_line(line):
+def get_breadcrumb(view, points, limit):
+  for pt in points:
     ch = view.substr(pt)
     if not ch.isspace():
-      return view.substr(sublime.Region(pt,min(line.b,pt + limit))).strip()
+      return view.substr(sublime.Region(pt, min(view.line(pt).b, pt + limit))).strip()
   return ''
 
 class BreadcrumbsCommand(sublime_plugin.EventListener):
-    def on_selection_modified(self, view):
-        settings = sublime.load_settings('Breadcrumbs.sublime-settings')
-        breadcrumb_length_limit = settings.get('breadcrumb_length_limit', 100)
-        separator = settings.get('breadcrumbs_separator', u' ■ ')
-        total_breadcrumbs_length_limit = settings.get('total_breadcrumbs_length_limit', 200)
-        view.erase_status('breadcrumbs')
-        current_row = None
-        for region in view.sel():
-          current_row = view.rowcol(region.b)[0]
+  def on_selection_modified(self, view):
+    tab_size = get_tab_size(view)
+    settings = sublime.load_settings('Breadcrumbs.sublime-settings')
+    breadcrumb_length_limit = settings.get('breadcrumb_length_limit', 100)
+    separator = settings.get('breadcrumbs_separator', u' ■ ')
+    total_breadcrumbs_length_limit = settings.get('total_breadcrumbs_length_limit', 200)
+    view.erase_status('breadcrumbs')
+
+    if len(view.sel()) == 0:
+      return
+
+    current_row = view.rowcol(view.sel()[0].b)[0]
+
+    def get_row_start(row):
+      return view.text_point(row,0)
+
+    def get_points_by_row(row):
+      return xrange(get_row_start(row), get_row_start(row + 1))
+
+    if is_white_row(view, get_points_by_row(current_row)):
+      while 0 <= current_row and is_white_row(view, get_points_by_row(current_row)):
+        current_row -= 1
+
+      if current_row < 0:
+        return
+
+      indentation = get_row_indentation(get_points_by_row(current_row), view, tab_size) + 1
+    else:
+      indentation = get_row_indentation(get_points_by_row(current_row), view, tab_size)
+      current_row -= 1
+
+    breadcrumbs = []
+    last_line_start = view.text_point(current_row + 1, 0)
+    while 0 <= current_row and 0 < indentation:
+      line_start = get_row_start(current_row)
+      points = xrange(line_start, last_line_start)
+      last_line_start = line_start
+      current_indentation = get_row_indentation(points, view, tab_size, indentation)
+      if current_indentation < indentation and not is_white_row(view, points):
+        indentation = current_indentation
+        breadcrumbs.append(get_breadcrumb(view, points, breadcrumb_length_limit))
+
+      current_row -= 1
+
+    breadcrumbs.reverse()
+    lengths = [len(breadcrumb) for breadcrumb in breadcrumbs]
+    sorted_lengths = sorted(lengths)
+    previous_length = 0
+    number_of_characters_left  = total_breadcrumbs_length_limit - len(lengths) * len(separator)
+    for (number_of_shorter, current_length) in enumerate(sorted_lengths):
+      if previous_length < current_length:
+        previous_length = current_length
+        number_of_not_shorter = len(breadcrumbs) - number_of_shorter
+        if number_of_characters_left < current_length * number_of_not_shorter:
+          length_of_short_trim = number_of_characters_left // number_of_not_shorter
+          length_of_long_trim = length_of_short_trim + 1
+          number_of_long_trimmed = number_of_characters_left % number_of_not_shorter
+          number_of_short_trimmed = number_of_not_shorter - number_of_long_trimmed
+          for (index_of_breadcrumb, breadcrum_length) in enumerate(lengths):
+            if current_length <= breadcrum_length:
+              if 0 < number_of_short_trimmed:
+                length_of_trim = length_of_short_trim
+                number_of_short_trimmed -= 1
+              else:
+                length_of_trim = length_of_long_trim
+              breadcrumbs[index_of_breadcrumb] = breadcrumbs[index_of_breadcrumb][0:length_of_trim]
           break
-
-        if current_row is None:
-          return
-
-        if is_white_row(view, current_row):
-          while 0 <= current_row and is_white_row(view, current_row):
-            current_row -= 1
-
-          if current_row < 0:
-            return
-
-          indentation = get_row_indentation(view, current_row) + 1          
-        else:
-          indentation = get_row_indentation(view, current_row)
-          current_row -= 1
-
-        breadcrumb_lines = []
-        while 0<=current_row and 0<indentation:
-          if not is_white_row(view, current_row):
-            current_indentation = get_row_indentation(view, current_row)
-
-            if current_indentation < indentation:
-              indentation = current_indentation
-              breadcrumb_lines.append(get_line_by_row(view,current_row))
-
-          current_row -= 1
-
-        breadcrumbs = [
-          get_breadcrumb(view, breadcrumb_line, breadcrumb_length_limit)
-          for breadcrumb_line in reversed(breadcrumb_lines)
-        ]
-
-        lengths = [len(breadcrumb) for breadcrumb in breadcrumbs]
-        sorted_lengths = sorted(lengths)
-        previous_length = 0
-        number_of_characters_left  = total_breadcrumbs_length_limit - len(lengths) * len(separator)
-        for (number_of_shorter, current_length) in enumerate(sorted_lengths):
-          if previous_length < current_length:
-            previous_length = current_length
-            number_of_not_shorter = len(breadcrumbs) - number_of_shorter
-            if number_of_characters_left < current_length * number_of_not_shorter:
-              length_of_short_trim = number_of_characters_left // number_of_not_shorter
-              length_of_long_trim = length_of_short_trim + 1
-              number_of_long_trimmed = number_of_characters_left % number_of_not_shorter
-              number_of_short_trimmed = number_of_not_shorter - number_of_long_trimmed
-              for (index_of_breadcrumb, breadcrum_length) in enumerate(lengths):
-                if current_length <= breadcrum_length:
-                  if 0 < number_of_short_trimmed:
-                    length_of_trim = length_of_short_trim
-                    number_of_short_trimmed -= 1
-                  else:
-                    length_of_trim = length_of_long_trim
-                  breadcrumbs[index_of_breadcrumb] = breadcrumbs[index_of_breadcrumb][0:length_of_trim]
-              break
-          number_of_characters_left -= current_length
-
-        view.set_status('breadcrumbs',separator.join(breadcrumbs+['']))
-
+      number_of_characters_left -= current_length
+    view.set_status('breadcrumbs',separator.join(breadcrumbs+['']))
